8d53a54056c71f712f086ce31704092beaccfae7
chuong2v <chuong.2v@gmail.com>
2015-12-31 15:57:16 +0700

    fix error delete list

--- routes/lists/delete.js
+++ routes/lists/delete.js
@@ -2,9 +2,19 @@ var List = require('../../models/list');
 var Todo = require('../../models/todo');
 
 module.exports = function(req, res) {
-  List.remove({
-    _id: req.params.id
+  Todo.remove({
+    listId: req.params.id
   }, function(err) {
-    if (err) return handleError(err);
-  });
+    if (err){
+      console.log("loi cmnr");
+      res.status(500).send(err);
+    }
+    else {
+      List.remove({
+        _id: req.params.id
+      }, function(err) {
+        if (err) return handleError(err);
+      });
+    }
+  })
 }
